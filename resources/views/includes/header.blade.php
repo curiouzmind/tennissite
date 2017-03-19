1109f3c8160b853ea5a9ab3e33859d1751464a12
Bolaji Alade <bolajialade.work@gmail.com>
2017-03-19 03:12:41 +0000

    test p2d

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -34,7 +34,7 @@
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Matches <span class="caret"></span></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Match <span class="caret"></span></a>
                     <ul class="dropdown-menu">
                         <li><a href="#">Scores & Stats</a></li>
                         <li><a href="#">Draws</a></li>
